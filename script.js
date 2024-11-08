de0fc4dc15f6baf2011186904302c59b87308ef6
Ayman-121 <107408891+Ayman-121@users.noreply.github.com>
2024-11-08 18:53:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,35 +7,35 @@ function updateNthTermInfo(firstValue, rateOfChange, nthTerm) {
   document.getElementById('nth-term-info').innerText = `The ${nthTerm}th term is: ${nthValue}`;
 
   if (document.getElementById('sum-checkbox').checked) {
-      const sum = (nthTerm / 2) * (2 * firstValue + (nthTerm - 1) * rateOfChange);
-      document.getElementById('nth-term-info').innerText += `, Sum: ${sum}`;
+    const sum = (nthTerm / 2) * (2 * firstValue + (nthTerm - 1) * rateOfChange);
+    document.getElementById('nth-term-info').innerText += `, Sum: ${sum}`;
   }
 }
 
-document.getElementById('sequence-input').addEventListener('input', function() {
+document.getElementById('sequence-input').addEventListener('input', function () {
   const inputText = this.value.replace(/[^0-9,-\s]/g, ''); // Allow only numbers, commas, and spaces as commas
   this.value = inputText.replace(/\s+/g, ',');
 
   const sequence = this.value.split(',').map(Number);
   if (sequence.length > 1 && isArithmeticSequence(sequence)) {
-      const firstValue = sequence[0];
-      const rateOfChange = sequence[1] - sequence[0];
+    const firstValue = sequence[0];
+    const rateOfChange = sequence[1] - sequence[0];
 
-      document.getElementById('sequence-info').innerText = `First value: ${firstValue}, Rate of change: ${rateOfChange}`;
+    document.getElementById('sequence-info').innerText = `First value: ${firstValue}, Rate of change: ${rateOfChange}`;
 
-      const nthTerm = parseInt(document.getElementById('nth-term-input').value);
-      if (!isNaN(nthTerm)) {
-          updateNthTermInfo(firstValue, rateOfChange, nthTerm);
-      } else {
-          document.getElementById('nth-term-info').innerText = '';
-      }
-  } else {
-      document.getElementById('sequence-info').innerText = 'Please enter a valid arithmetic sequence.';
+    const nthTerm = parseInt(document.getElementById('nth-term-input').value);
+    if (!isNaN(nthTerm)) {
+      updateNthTermInfo(firstValue, rateOfChange, nthTerm);
+    } else {
       document.getElementById('nth-term-info').innerText = '';
+    }
+  } else {
+    document.getElementById('sequence-info').innerText = 'Please enter a valid arithmetic sequence.';
+    document.getElementById('nth-term-info').innerText = '';
   }
 });
 
-document.getElementById('nth-term-input').addEventListener('input', function() {
+document.getElementById('nth-term-input').addEventListener('input', function () {
   const inputText = document.getElementById('sequence-input').value;
   const sequence = inputText.split(',').map(Number);
   const firstValue = sequence[0];
@@ -43,13 +43,13 @@ document.getElementById('nth-term-input').addEventListener('input', function() {
 
   const nthTerm = parseInt(this.value);
   if (!isNaN(nthTerm)) {
-      updateNthTermInfo(firstValue, rateOfChange, nthTerm);
+    updateNthTermInfo(firstValue, rateOfChange, nthTerm);
   } else {
-      document.getElementById('nth-term-info').innerText = '';
+    document.getElementById('nth-term-info').innerText = '';
   }
 });
 
-document.getElementById('sum-checkbox').addEventListener('change', function() {
+document.getElementById('sum-checkbox').addEventListener('change', function () {
   const inputText = document.getElementById('sequence-input').value;
   const sequence = inputText.split(',').map(Number);
   const firstValue = sequence[0];
@@ -57,6 +57,6 @@ document.getElementById('sum-checkbox').addEventListener('change', function() {
 
   const nthTerm = parseInt(document.getElementById('nth-term-input').value);
   if (!isNaN(nthTerm)) {
-      updateNthTermInfo(firstValue, rateOfChange, nthTerm);
+    updateNthTermInfo(firstValue, rateOfChange, nthTerm);
   }
 });
